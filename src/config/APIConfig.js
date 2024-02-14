1780b91f193062dbf5c065477c96c0c870911432
Mario Martín <mmartin.mrmg@gmail.com>
2024-02-14 09:16:28 +0100

    feat: ✨ Modify API Config

--- src/config/APIConfig.js
+++ src/config/APIConfig.js
@@ -1,4 +1,4 @@
 //YOU MUST MODIFY THIS PARAMS WITH YOUR API
 export const API_URL = "http://localhost/DWES/API-Connect/books"
-export const TABLE = ""
+export const TABLE = "books"
 export const FILTERS = ['ID' , 'Autor', 'Genero', 'Pagina', 'Todos']
